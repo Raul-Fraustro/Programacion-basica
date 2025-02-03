27d77f2b643376f92a85874c02b5a33477065fdc
Raul-Fraustro <rfraustrom1909@gmail.com>
2025-02-03 00:56:52 -0600

    Ejercicio2

---
+++ Semana 1/Ejercicio2.py
@@ -0,0 +1,9 @@
+num1 = int(input("Introduca el primer numero: "))
+num2 = int(input("Introduca el segundo numero: "))
+
+if num1>num2:
+    print(num1,"Es mayor")
+elif num1<num2:
+    print(num2, "Es menor")
+else:
+    print("Son iguales")
