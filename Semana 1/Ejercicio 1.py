f6185083f1a9e2ea4a7f20d87c9082fd1b369f70
Raul-Fraustro <rfraustrom1909@gmail.com>
2025-02-03 00:49:10 -0600

    ejercicio1

---
+++ Semana 1/Ejercicio 1.py
@@ -0,0 +1,9 @@
+num1 = int(input("Introduzca el primer numero: "))
+num2 = int(input("Introduzca el segundo numero: "))
+
+print("suma: ", num1+num2)
+print("resta: ", num1-num2)
+print("multiplicacion: ", num1*num2)
+print("division: ", num1/num2)
+print("dividion de piso: ", num1//num2)
+print("residuo: ", num1%num2)
