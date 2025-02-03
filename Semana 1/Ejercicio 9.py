64f6c5f48e2ff3366bd747a767c3cc148c884245
Raul-Fraustro <rfraustrom1909@gmail.com>
2025-02-03 01:59:45 -0600

    Ejercicio 9

---
+++ Semana 1/Ejercicio 9.py
@@ -0,0 +1,7 @@
+cal1 = float(input("Introduce la primera calificacion: "))
+cal2= float(input("Introduce la segunda calificacion: "))
+cal3=float(input("Introduce la tercera calificacion: "))
+
+prom = (cal1+cal2+cal3)/3
+
+print("Promedio", prom)
