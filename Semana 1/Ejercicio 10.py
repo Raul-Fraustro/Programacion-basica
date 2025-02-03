92603c5748712f7f76e59b9038eede7ff24b433b
Raul-Fraustro <rfraustrom1909@gmail.com>
2025-02-03 02:05:09 -0600

    Ejercicio 10

---
+++ Semana 1/Ejercicio 10.py
@@ -0,0 +1,6 @@
+base = float(input("Introduce la base del triangulo: "))
+altura = float(input("Introduce la altura del triangulo: "))
+
+area = base*altura/2
+
+print("El area del triangulo es: ", area)
